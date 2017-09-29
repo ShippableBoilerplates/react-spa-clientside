ff7a96d17cac0080ab7e216bd0d2ba8c82fed6b9
levsthings <bearnode@gmail.com>
2017-09-29 22:56:33 +0300

    refactor sagas

--- src/client/js/sagas/index.js
+++ src/client/js/sagas/index.js
@@ -1,32 +1,21 @@
 import {takeEvery} from 'redux-saga'
 import {call, put} from 'redux-saga/effects'
-import fetch from 'isomorphic-fetch'
+import {fetchAJoke} from '../api'
 import constants from '../constants'
 
-// worker saga
 function* getChuckNorrisJokeAsync(action) {
     try {
-        const apiCall = () => {
-            return fetch('http://api.icndb.com/jokes/random')
-                .then(res => res.json())
-                .then(res => res.value.joke)
-        }
-        const response = yield call(apiCall)
-
-        yield put({type: constants.GET_CHUCK_NORRIS_JOKE, payload: response})
+        const joke = yield call(fetchAJoke)
+        yield put({type: constants.GET_CHUCK_NORRIS_JOKE, payload: joke})
     } catch (error) {
-        // return error
         console.log(error)
     }
 }
 
-// watcher saga - spawn new async on each ACTION
 function* watchClick() {
     yield takeEvery(constants.UPDATE_CLICK_COUNT, getChuckNorrisJokeAsync)
 }
 
-// root saga
-// single entry point to start all sagas at once
 export default function* rootSaga() {
     yield [
         watchClick()

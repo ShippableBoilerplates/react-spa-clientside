24892a71d9a53a3553d0cc58a041a18f04968e6f
levsthings <bearnode@gmail.com>
2017-09-29 22:31:18 +0300

    update eslint rules for generator funcs and update deps

--- src/client/js/sagas/index.js
+++ src/client/js/sagas/index.js
@@ -1,10 +1,10 @@
 import {takeEvery} from 'redux-saga'
 import {call, put} from 'redux-saga/effects'
-import constants from '../constants'
 import fetch from 'isomorphic-fetch'
+import constants from '../constants'
 
 // worker saga
-export function * getChuckNorrisJokeAsync(action) {
+function* getChuckNorrisJokeAsync(action) {
     try {
         const apiCall = () => {
             return fetch('http://api.icndb.com/jokes/random')
@@ -15,18 +15,19 @@ export function * getChuckNorrisJokeAsync(action) {
 
         yield put({type: constants.GET_CHUCK_NORRIS_JOKE, payload: response})
     } catch (error) {
+        // return error
         console.log(error)
     }
 }
 
 // watcher saga - spawn new async on each ACTION
-export function * watchClick() {
+function* watchClick() {
     yield takeEvery(constants.UPDATE_CLICK_COUNT, getChuckNorrisJokeAsync)
 }
 
 // root saga
 // single entry point to start all sagas at once
-export default function * rootSaga() {
+export default function* rootSaga() {
     yield [
         watchClick()
     ]

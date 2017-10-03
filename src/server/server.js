777ddff9d8948469c1ca56af4bc4935cfa202543
levsthings <bearnode@gmail.com>
2017-10-03 18:24:47 +0300

    remove compression from node

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,4 @@
 'use strict'
-// const compression = require('compression')
 const express = require('express')
 const helmet = require('helmet')
 const morgan = require('morgan')
@@ -13,14 +12,13 @@ const publicFolder = path.join(__dirname, '../public')
 const index = path.join(publicFolder, '/index.html')
 
 /**
- * Middleware for compression, security, logging, and serving static content
+ * Middleware for security, logging, and serving static content
  */
 const app = express()
 
 app.disable('x-powered-by')
 app.use(helmet())
 app.use(morgan('tiny'))
-// app.use(compression())
 app.use(express.static(publicFolder))
 
 /**
